b9be463bd7680af3042196607cd856216535cbe4
DJ <delroyjordancos@gmail.com>
2022-11-28 10:11:34 -0500

    upodate to grid board and placement

---
+++ Assets/WarRoom/Assets/Scripts/InputManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    //get reference to game grid
+    Game_Grid gameGrid;
+    [SerializeField] private LayerMask whatIsAGridLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameGrid = FindObjectOfType<Game_Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GridCell cellMouseIsOver = isMouseOverAGridSpace();
+        if(cellMouseIsOver != null)
+        {
+            if(Input.GetMouseButtonDown(0)){
+                cellMouseIsOver.GetComponentInChildren<SpriteRenderer>().material.color = Color.green;
+
+            }
+        }
+    }
+
+    // returns the grid cell if tghe mouse is over it or null if not
+
+    private GridCell isMouseOverAGridSpace()
+    {
+        //ray cast to camera through mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, 100F, whatIsAGridLayer))
+        {
+            return hitInfo.transform.GetComponent<GridCell>();
+        }
+        else {
+            return null;
+        }
+    }
+}
