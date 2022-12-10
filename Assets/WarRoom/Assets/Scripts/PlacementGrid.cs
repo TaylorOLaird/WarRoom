9bd44b3c6f1e54790f189a051a1f338b8be95841
DJ <delroyjordancos@gmail.com>
2022-12-09 21:05:27 -0500

    update to grids

--- Assets/WarRoom/Assets/Scripts/PlacementGrid.cs
+++ Assets/WarRoom/Assets/Scripts/PlacementGrid.cs
@@ -10,6 +10,7 @@ public class PlacementGrid : MonoBehaviour
     private int height = 13;
     private int width = 13;
     private float GridSpaceSize = 2.5f;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +27,18 @@ public class PlacementGrid : MonoBehaviour
             Debug.LogError("ERROR: Grid Cell Prefab on the Game Grid Prefab is not assigned");
         }
 
-        // create a 13 x 13 grid of grid elements
-        for (int x = 0; x < 13; x++)
+        // create a 10 x 10 grid of grid elements
+        for (int x = 0; x < 10; x++)
         {
-            for (int y = 0; y < 13; y++)
+            for (int y = 0; y < 10; y++)
             {
                 // create a new grid element
-                gameGridSmall[x,y] = Instantiate(gridElementPrefab);
-                gameGridSmall[x,y].transform.parent = transform;
-                gameGridSmall[x,y].transform.localPosition = new Vector3(x * GridSpaceSize/10, 0, y * GridSpaceSize/10);
-                gameGridSmall[x,y].transform.rotation = Quaternion.identity;
+                gameGridSmall[x,y] = Instantiate(gridElementPrefab, transform);
+                // gameGridSmall[x,y].transform.parent = transform;
+                gameGridSmall[x,y].transform.localPosition = new Vector3(x, 0.00001f, y);
+                // scale the grid element to fit the grid
+                // gameGridSmall[x,y].transform.localScale = new Vector3(GridSpaceSize/10, GridSpaceSize/10, GridSpaceSize/10);
+                // gameGridSmall[x,y].transform.rotation = Quaternion.identity;
 
                 if(gameGridSmall[x, y].GetComponent<GridCell>() == null){
                     Debug.LogError("No component Found");
@@ -44,4 +47,6 @@ public class PlacementGrid : MonoBehaviour
             }
         }
     }
+    // function to make grid element prefab highlight on hover
+
 }
